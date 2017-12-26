d2071f23a903c89965debbab5696d5410d126355
Tomas <jeldicek@seznam.cz>
2017-12-26 21:19:04 +0100

    extend controller

--- src/Controller/LuckyController.php
+++ src/Controller/LuckyController.php
@@ -2,10 +2,11 @@
 
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class LuckyController
+class LuckyController extends Controller
 {
     /**
      * @Route("/lucky/number")
