af1061ce241b3e2a6ec2027b4cb3f5f5c136cb7e
Tomas <jeldicek@seznam.cz>
2018-01-02 13:06:05 +0100

    pred konfiguraci pro controllery

--- src/Controller/ReportPlaceController.php
+++ src/Controller/ReportPlaceController.php
@@ -2,45 +2,17 @@
 
 namespace App\Controller;
 
-use App\Entity\MeasurePlace;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Request;
-//use App\Form\MeasurePlaceType;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Routing\Annotation\Route;
 
-class ReportPlaceController extends AbstractController
+class ReportPlaceController extends Controller
 {
     /**
      * @Route("/nahlasit-misto", name="reportPlace")
      */
-    public function newPlace(Request $request)
+    public function reportPlace()
     {
-        $title = 'Nahlásit místo';
-
-        $measurePlace = new MeasurePlace();
-
-        /*$form = $this->createForm(MeasurePlaceType::class, $measurePlace);
-
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            $place = $form->getData();
-            $em    = $this->getDoctrine()->getManager();
-            $em->persist($place);
-            $em->flush();
-
-            $this->addFlash(
-                'notice',
-                'Děkujeme za nahlášení! Zanedlouho místo zveřejníme.'
-            );
-
-            return $this->redirectToRoute('reportPlace');
-        }*/
-
         return $this->render('reportPlace.html.twig', [
-            'title' => $title,
-           // 'form'  => $form->createView(),
         ]);
-
     }
 }
